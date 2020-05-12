d7e6d4fdc006af6310533912aeb8c51e2526ba41
Joao Pinto <jdpinto@me.local>
2020-05-12 03:57:27 +0100

    Updates

---
+++ precache-manifest.5c4e8aa1fec5fa5d2e63203f5940a734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2fa4cdb7024db1b68ce00be2a84d8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fba944f7a94e836867a7",
+    "url": "/static/css/main.319321e9.chunk.css"
+  },
+  {
+    "revision": "36bab8ccdb7f487776e2",
+    "url": "/static/js/2.ba5937f2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ba5937f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fba944f7a94e836867a7",
+    "url": "/static/js/main.fe1304d3.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
